3604ff9b5f84c68583f1babdc34b216441d6a8b3
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-02 10:26:02 -0500

    created interface and object

---
+++ src/lib/posts.ts
@@ -0,0 +1,29 @@
+export interface Post {
+    id: number;
+  slug: string;
+  title: string;
+  content: string;
+}
+
+export const post : Post[] = [
+    {
+         id: 1,
+  slug: "my-first-post",
+  title: "My First Post",
+  content: "This is the content of my first blog post."
+    },
+    {
+        id: 2,
+    slug: "react-router-guide",
+    title: "React Router Guide",
+    content: "Learn how dynamic routing works in React Router."
+    },
+    
+    {        
+    id: 3,
+    slug: "context-api-explained",
+    title: "Context API Explained",
+    content: "This article explains how to use React Context for global state."
+    },
+
+]
